52ba63b3903c75f8fe83668e02384e59e02aded1
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 05:03:24 +0100

    Gitacora saved 2741 changed files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Homepage from './components/Homepage';
+import Homepage from './components/Homepage'
 import Register from './components/auth/Register'
 
 const App = () => {
