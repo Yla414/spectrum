a161e9e33fe03fabc1538e4301e3e82c9f5b7ebb
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 01:27:47 +0100

    Gitacora saved 4 changed files

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -32,7 +32,7 @@ const register = async (req, res) => {
 const login = async (req, res) => {
     try {
         // Validate the credentials
-        const user
+        const user = U
     } catch (error) {
         
     }
