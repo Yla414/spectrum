ba76305eef0581f1c669c48049e40871c3a2ef16
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 19:43:16 +0100

    Gitacora saved 2744 changed files

--- client/src/pages/dashboard/SellerDashboard.jsx
+++ client/src/pages/dashboard/SellerDashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux';
 
 const SellerDashboard = () => {
-    const user = useSelector((state) => state.auth.user)
+    const user = useSelector((state) => state.auth.user);
   return (
     <div>
       <h2>Welcome to the Seller dashboard!</h2>
