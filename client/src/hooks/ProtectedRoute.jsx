243c76c067840f784f48470dc85555803887ec6d
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 18:55:12 +0100

    Gitacora saved 2743 changed files

--- client/src/hooks/ProtectedRoute.jsx
+++ client/src/hooks/ProtectedRoute.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { useNavigate, Route } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const useProtectedRoute = (allowedRoles) => {
     const user = useSelector((state) => state.auth.user);
     const navigate = useNavigate();
-    
+
     if(!user || !allowedRoles.includes(user.role)) {
         return navigate('/login');
     }
