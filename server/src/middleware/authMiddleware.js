84510c636fd8040b15f339ee9fa5f750d68862f1
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 02:29:29 +0100

    Gitacora saved 7 changed files

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -5,7 +5,7 @@ const authenticate = (req, res, next) => {
         req.user = decode;
         next();
     } catch (error) {
-        res.status(500).json({  })
+        res.status(500).json({ err })
     }
     
 }
