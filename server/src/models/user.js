44e75379b1252bdaf3797207f572e6ab2ff46689
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 01:30:35 +0100

    Gitacora saved 4 changed files

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         enum: ['Guest', 'Seller', 'Creator'],
         default: 'Guest',
-        requir
+        required: true
     }
 });
 
