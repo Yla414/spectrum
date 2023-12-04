02d22f6af1707fbf13b0e3d237c40f99de4ab8a4
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 20:08:20 +0100

    Gitacora saved 2 changed files

--- client/src/components/Logout.jsx
+++ client/src/components/Logout.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom'
 
 const Logout = () => {
     const dispatch = useDispatch();
-    const navigate
+    const navigate 
 
     const handleLogout = () => {
         dispatch(logout());
