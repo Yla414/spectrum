07b157153bfa66281cb27698c07c5c2798893e4a
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 01:01:45 +0100

    Gitacora saved 1 changed files

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import store from './r'
+import store from './red'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
