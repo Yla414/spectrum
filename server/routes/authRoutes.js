fa5b3f54648125b6c84f02b9766b5d114d5c9715
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 04:08:14 +0100

    Gitacora saved 2739 changed files

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -11,7 +11,7 @@ router.post('/register', async (req, res) => {
         const { username, email, password, role } = req.body;
 
         // Check for existing user
-        const existingUser = await 
+        const existingUser = await User.findOne
     } catch (error) {
 
     }
