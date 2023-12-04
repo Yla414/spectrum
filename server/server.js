25f62d28b9ab98719d3859909e78278e279439d1
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 21:06:27 +0100

    Gitacora saved 3 changed files

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ connectDB();
 
 // Routes
 app.use('/api/auth', authRoutes);
-app.use('/api')
+app.use('/api/')
 
 app.listen(process.env.PORT, () => {
     console.log(`Connected to http://localhost:${process.env.PORT}`)
