bb4948976650cedaada49341754e5df41bb2b15b
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 03:09:25 +0100

    Gitacora saved 7 changed files

--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -17,7 +17,7 @@ const Register = () => {
     <div>
       <form onSubmit={handleRegister}>
         <label>
-            Username
+            Username:
         </label>
       </form>
     </div>
