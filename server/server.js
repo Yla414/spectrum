e8b5aec1c4a983f95944ecc19ed0038883908631
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 04:01:38 +0100

    Gitacora saved 2741 changed files

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+// Express app
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Env configuration
+dotenv.config();
+
+connectDB();
+
+app.listen(process.env.PORT, () => {
+    console.log(`Connected to http://localhost:${process.env.PORT}`)
+})
