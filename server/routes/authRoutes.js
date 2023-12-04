6cf101724bb484b92fa782156c2a17fc8b90db1f
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 19:26:25 +0100

    Gitacora saved 2744 changed files

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 const dotenv = require('dotenv')
 
-dotenv.config
+dotenv.config();
 
 // REGISTRATION ROUTE
 router.post('/register', async (req, res) => {
