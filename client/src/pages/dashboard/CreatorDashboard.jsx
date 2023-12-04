6f6549c736b6ea6bf1ae05115ecbf3b6cfb9e693
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 21:15:38 +0100

    Gitacora saved 3 changed files

--- client/src/pages/dashboard/CreatorDashboard.jsx
+++ client/src/pages/dashboard/CreatorDashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 import Logout from '../../components/Logout';
-import { fetchU }
+import { fetchUs }
 
 const CreatorDashboard = () => {
   const user = useSelector((state) => state.auth.user);
