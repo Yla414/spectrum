bbddbb78db4814daafd2c9b6f7200a6bf9907b51
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 04:17:56 +0100

    Gitacora saved 2739 changed files

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -39,7 +39,7 @@ router.post('/login', async (req, res) => {
         const { email, password } = req.body;
 
         // fIND USER BY EMAIL
-        const user = User.find
+        const user = User.findOne({  })
     }
 });
 
