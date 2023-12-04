fad17b3bac909135a57d5c329862eb1d9f552174
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 20:04:41 +0100

    Gitacora saved 2 changed files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -35,7 +35,7 @@ const App = () => {
         dispatch(setToken(storedToken));
       })
       .catch((error) => {
-        console.error('Error fe')
+        console.error('Error fetching')
       })
     }
   }, [])
