01eb9c4199c5d189640114ef68debbf5d230df8e
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 04:10:35 +0100

    Gitacora saved 2739 changed files

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -20,7 +20,7 @@ router.post('/register', async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, 10);
 
         // Create a new user
-        const newUser = new User({})
+        const newUser = new User({ username })
     } catch (error) {
 
     }
