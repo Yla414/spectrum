c74bf6a004cb92291458c630143f65d3907fa966
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 18:51:19 +0100

    Gitacora saved 2743 changed files

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import useProtectedRoute from '../components/ProtectedRoute'
+import useProtectedRoute from '../hooks/ProtectedRoute'
 
 const Dashboard = () => {
     const user = useSelector((state) => state.auth.user);
