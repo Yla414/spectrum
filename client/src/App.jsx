b09509ca9c6f6605d4db4606e2ea60adc67227ce
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 19:59:50 +0100

    Gitacora saved 2 changed files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import { Homepage, Register, Login, SellerDashboard, CreatorDashboard, GuestDash
 
 const App = () => {
   const userRole = useSelector(state => state.auth.user?.role)
-  
+  ;
   const GetDashboardComponent = () => {
     switch (userRole) {
       case 'Seller':
