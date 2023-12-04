174c6f9376dfeb09c8769df8181e4d56684b46f2
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 03:56:50 +0100

    Gitacora saved 2739 changed files

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,4 +2,8 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const User = require('../models/')
+const User = require('../models/User');
+
+
+// REGISTRATION ROUTE
+router.post('/register', (req, res))
