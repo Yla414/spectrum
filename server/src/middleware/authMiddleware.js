b3c7e876c6ae796466cf5305f84f1b7c14a9990d
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 02:38:47 +0100

    Gitacora saved 7 changed files

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -7,7 +7,7 @@ const authenticate = (req, res, next) => {
             throw new Error('Authentication failed');
         }
         
-        const decoded = jwt.verify(token, process.env.JWT_)
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
         req.user = decode;
         next();
     } catch (error) {
