d89ec3e1c89532a583f8a770b8570f74d02985d7
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 17:40:10 +0100

    Gitacora saved 2741 changed files

--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux';
-import { setUser, setToken } from '../'
+import { setUser, setToken } from '../../'
 
 const Register = () => {
     const navigate = useNavigate();
