44af905f1b21cdd8680a241e1e013044f9d4c431
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 20:07:34 +0100

    Gitacora saved 2 changed files

---
+++ client/src/components/Logout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { logout } from '../redux/auth/authSlice'
+
+const Logout = () => {
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {}
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Logout
