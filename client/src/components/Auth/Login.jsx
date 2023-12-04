bc8efb28ca86b28b54685d9ab28bd55f9b26b4c7
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 20:17:31 +0100

    Gitacora saved 2 changed files

--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -48,11 +48,11 @@ const Register = () => {
         </label> */}
         <label>
             Username:
-            <input type="text" name="username" autoComplete='p' value={username} onChange={(e) => setUsername(e.target.value)} />
+            <input type="text" name="username" autoComplete='off' value={username} onChange={(e) => setUsername(e.target.value)} />
         </label>
         <label>
             Password:
-            <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <input type="password" name="password"  value={password} onChange={(e) => setPassword(e.target.value)} />
         </label>
 
         <button type="submit">Login</button>
