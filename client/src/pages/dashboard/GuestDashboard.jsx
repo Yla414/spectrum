19b49dc32f2435b09033c841e91a4b9518763c6a
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 20:11:24 +0100

    Gitacora saved 2 changed files

--- client/src/pages/dashboard/GuestDashboard.jsx
+++ client/src/pages/dashboard/GuestDashboard.jsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import Logout from '../../components/Logout';
 
 const Dashboard = () => {
+    
     return (
         <div>
             <h2>Welcome to the guest dashboard!</h2>
