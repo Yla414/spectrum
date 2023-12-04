5842a7f5ff098284620771ec0c381b4c36f2f778
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 02:07:59 +0100

    Gitacora saved 8 changed files

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -43,7 +43,8 @@ router.post('/login', sync (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal server error' });
-    })
+    }
+)
 
 
 module.exports = router;
