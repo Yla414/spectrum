c105eea0402b00852677e6bb4ed9c00b773c0c17
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 18:49:02 +0100

    Gitacora saved 2742 changed files

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import 
 
 const Dashboard = () => {
-    const user = useSelector((state) => state.auth.user);
     return (
         <div>
             <h2>Welcome to the dashboard, {user.username}!</h2>
