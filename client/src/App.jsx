02f024dbe4615bad3c703090e3c03a3fb39a7c61
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 20:03:04 +0100

    Gitacora saved 2 changed files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,7 +28,7 @@ const App = () => {
           Authorization: `Bearer ${storedToken}`,
         }
       }).then((res) => {
-        const { user } = 
+        const { user } = response
       })
     }
   }, [])
