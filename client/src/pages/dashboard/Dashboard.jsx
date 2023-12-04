0e43bd5d806da5fcb5bc37e83e239ac1b537029d
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 19:01:31 +0100

    Gitacora saved 2744 changed files

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -20,7 +20,7 @@ const Dashboard = () => {
     
     return (
         <div>
-            <h2>Welcome to the dashboard, {user.email}!</h2>
+            <h2>Welcome to the dashboard, {.email}!</h2>
             <p>Your role: {role}</p>
         </div>
     )
