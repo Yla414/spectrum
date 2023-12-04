fa18ada4981d2b334d60b933a524c160a2e77297
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 02:04:07 +0100

    Gitacora saved 6 changed files

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 
 const authRoutes = require('./src/routes/authRoutes');
-const productRoute = require('./src/routes/productRoutes');
+const productRoutes = require('./src/routes/productRoutes');
 
 const app = express();
 dotenv.config();
