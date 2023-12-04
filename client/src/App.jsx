4a3d6ec49c60bee2110e8b9fa042cc394586bd45
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 18:13:20 +0100

    Gitacora saved 2741 changed files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Homepage from './components/Homepage'
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
 import Dashboard from './pages/dashboard/Dashboard';
-import ProtectedRou
+import ProtectedRoute from ''
 
 const App = () => {
   return (
