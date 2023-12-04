5f927b755fffa63ce0ffb5b369698a29960ccf32
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 18:59:02 +0100

    Gitacora saved 2744 changed files

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import useProtectedRoute from '../../hooks/useProtectedRoute'
-import {  }
+import { us }
 
 const Dashboard = () => {
     const redirect = useProtectedRoute(['Seller', 'Creator']);
