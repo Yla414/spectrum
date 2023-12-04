8625d98255fbe541e73823790835b044227d1d2f
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 19:05:15 +0100

    Gitacora saved 2744 changed files

---
+++ client/src/pages/dashboard/GuestDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h2>Welcome to the dashboard, {email}!</h2>
+            <p>Your role: {role}</p>
+        </div>
+    )
+}
+
+export default Dashboard;
