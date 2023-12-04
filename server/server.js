b4b62601523e25a3212f70ce5b5d6b14b484ca34
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 02:01:29 +0100

    Gitacora saved 6 changed files

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+const authRoutes = require('./src/routes/authRoutes');
+
+const app = express();
+dotenv.config();
+
+app.use('/auth', authRoutes);
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('MongoDB connection successful!');
+        app.listen(process.env.PORT, () => {
+            console.log(`Server connected to http://localhost:${process.env.PORT}`);
+        })
+    })
+    .catch((error) => console.log('Error connecting to MongoDB:', error));
