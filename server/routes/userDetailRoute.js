d513c54123e9191f27f3a8b51368b03d3cfb1741
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 21:01:20 +0100

    Gitacora saved 3 changed files

--- server/routes/userDetailRoute.js
+++ server/routes/userDetailRoute.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const jwt = require('jsonwebtoken');
-
+import
 const dotenv = require('dotenv');
 const authMiddleware = require('../middleware/authMiddleware');
 dotenv.config();
