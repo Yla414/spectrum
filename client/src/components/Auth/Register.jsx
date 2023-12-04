33f7dd2acbe6c2b238ace6d89a54f1ce220f6223
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 17:35:48 +0100

    Gitacora saved 2741 changed files

--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -21,7 +21,7 @@ const Register = () => {
             dispatch(setToken(response.data.token));
             console.log(response.data);
         } catch (error) {
-            console.error('Error registering user:', )
+            console.error('Error registering user:', error)
         }
     }
     
