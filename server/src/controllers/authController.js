9e617748e9c901f1160ff005e1f34eb9992af9c4
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 01:38:30 +0100

    Gitacora saved 4 changed files

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -50,8 +50,8 @@ const login = async (req, res) => {
         }
 
         const token = generateToken(user);
-        res.json({ to })
+        res.json({ token });
     } catch (error) {
-        
+        c
     }
 }
