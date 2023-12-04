f54245128df14569cdced3dbcb9f2650d143fe33
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 20:47:57 +0100

    Gitacora saved 3 changed files

--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -33,7 +33,7 @@ const Register = () => {
               dispatch(setUser(user));
               dispatch(setToken(token));
 
-              l
+              loca
 
               navigate('/dashboard');
         } catch (error) {
