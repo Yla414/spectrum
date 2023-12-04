6934d858a949894b6e5930cb981016b0db793f2d
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 01:38:00 +0100

    Gitacora saved 4 changed files

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -46,8 +46,9 @@ const login = async (req, res) => {
         const isValidPassword = await bcrypt.compare(req.body.password, user.password);
 
         if(!isValidPassword) {
-            return res.status(401).json({ error: 'Invalid username or password' })
+            return res.status(401).json({ error: 'Invalid username or password' });
         }
+        
     } catch (error) {
         
     }
