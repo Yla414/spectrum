4473b8cb19398238a79812465bba03c9a65dda3f
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 19:58:11 +0100

    Gitacora saved 1 changed files

---
+++ client/src/components/components.exports.js
@@ -0,0 +1,6 @@
+import Homepage from "./Homepage";
+import GuestDashboard from '../pages/dashboard/GuestDashboard'
+import CreatorDashboard from '../pages/dashboard/CreatorDashboard'
+import SellerDashboard from '../pages/dashboard/SellerDashboard'
+import Login from './auth/Login'
+import Register from './auth/Register'
