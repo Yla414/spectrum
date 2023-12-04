352175c58b56a54adff78027bb17dd865743c52f
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 02:58:14 +0100

    Gitacora saved 7 changed files

--- client/src/redux/auth/authSlice.js
+++ client/src/redux/auth/authSlice.js
@@ -32,7 +32,7 @@ export const selectToken = (state) => state.auth.token;
 
 export const register = (userData) => async (dispatch) => {
     try {
-        const res
+        const respo
     } catch (error) {
         
     }
