00be93ad712dbc15840c77d30d11ed5fc699e8ce
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 02:14:43 +0100

    Gitacora saved 6 changed files

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -28,7 +28,7 @@ const register = async (req, res) => {
 
         const token = generateToken(newUser);
         res.status(201).json({ token });
-        res.status(200).json({ message: "User reg" })
+        res.status(200).json({ message: "User registered su" })
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal server error' });
