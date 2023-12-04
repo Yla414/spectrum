1cd2c1cbbf2735b862ae7f0aa88b0de725172ae8
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 03:50:37 +0100

    Gitacora saved 2740 changed files

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique:  }
+    username: { type: String, required: true, unique: true },
 })
