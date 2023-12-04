f2784a05a218eda2826ce307569d8cf2d00c164b
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 02:11:54 +0100

    Gitacora saved 7 changed files

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -2,49 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.post('/register', async (req, res) => {
-    try {
-        // Validate and hash user password
-        const salt = 10;
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-        const newUser = await UserModel.create({
-            username: req.body.username,
-            password: hashedPassword,
-            email: req.body.email,
-            role: req.body.role,
-        });
-
-        const token = generateToken(newUser);
-        res.status(201).json({ token });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-});
-
-router.post('/login', async (req, res) => {
-    try {
-        // Validate the credentials
-        const user = UserModel.findOne({ username: req.body.username });
-
-        if(!user) {
-            return res.status(401).json({ error: 'Invalid username or password' });
-        }
-
-        const isValidPassword = await bcrypt.compare(req.body.password, user.password);
-
-        if(!isValidPassword) {
-            return res.status(401).json({ error: 'Invalid username or password' });
-        }
-
-        const token = generateToken(user);
-        res.json({ token });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-})
-
+router.post('/register', authController.register);
+router.post('/register', authController.login);
 
 module.exports = router;
