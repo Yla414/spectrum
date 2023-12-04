fcfc545a350e3115d272b2a07aebd0b7cb882a2d
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 01:23:41 +0100

    Gitacora saved 4 changed files

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -9,5 +9,5 @@ const generateToken = (user) => {
         role: user.role,
     };
 
-    return jwt.sign(payload, crypto.randomBytes(32).toString('hex'), { expiresIn: '' })
+    return jwt.sign(payload, crypto.randomBytes(32).toString('hex'), { expiresIn: '1' })
 }
