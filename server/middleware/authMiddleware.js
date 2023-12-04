6bbd37883084b395fb34353ecbc5b6da3d7dbfc9
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 20:56:04 +0100

    Gitacora saved 2 changed files

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -7,5 +7,5 @@ const authMiddleware = (req, res, next) => {
         return res.status(401).json({ message: 'Unathorized. No token provided!' });
     }
 
-    jwt.verify(token, process.env.JWT_SECRET)
+    jwt.verify(token, process.env.JWT_SECRET_)
 }
