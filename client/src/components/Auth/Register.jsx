940e3f7fb166b20fc1719df81b9b26a939304d8d
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 17:34:47 +0100

    Gitacora saved 2740 changed files

--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -18,7 +18,7 @@ const Register = () => {
 
             // Save user and the token generated to redux state
             dispatch(setUser(response.data.user));
-            di
+            dispatch(setToken)
         }
     }
     
