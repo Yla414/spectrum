40b499ff069936c3ac397cfb0fc5f92a821d0541
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 02:24:38 +0100

    Gitacora saved 7 changed files

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -5,6 +5,6 @@ const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
-router.get('/user', authM)
+router.get('/user', authMiddle)
 
 module.exports = router;
