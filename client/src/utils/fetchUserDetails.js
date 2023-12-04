85320adc9bce42906e30b840b8526ce182f826d6
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 21:13:42 +0100

    Gitacora saved 3 changed files

--- client/src/utils/fetchUserDetails.js
+++ client/src/utils/fetchUserDetails.js
@@ -5,7 +5,7 @@ const fetchUserDetails = async (dispatch) => {
     try {
         const response = await axios.get('/user', {
             headers: {
-                Authorization: 1
+                Authorization: 
             }
         })
     } catch (error) {
