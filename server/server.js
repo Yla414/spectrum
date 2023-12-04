84c218e41ed5453e10db593b04f30168954db3c7
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 21:31:47 +0100

    Gitacora saved 4 changed files

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:3']
+    origin: ['http://localhost:3000'],
 }));
 
 // Env configuration
