7753d85c9c181574af64f052d4fcc3eecd3970fe
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 21:14:29 +0100

    Gitacora saved 3 changed files

--- client/src/utils/fetchUserDetails.js
+++ client/src/utils/fetchUserDetails.js
@@ -9,7 +9,7 @@ const fetchUserDetails = async (dispatch) => {
             },
         });
 
-        const { user } = response.d
+        const { user } = response.data
     } catch (error) {
         
     }
