6fdfdb7d2aa68fce84ec48c78aaa68b6c0719577
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 04:26:12 +0100

    Gitacora saved 2739 changed files

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,67 +1,78 @@
-const express = require('express');
-const router = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
-
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
 
 // REGISTRATION ROUTE
 router.post('/register', async (req, res) => {
-    try {
-        const { username, email, password, role } = req.body;
+  try {
+    const { username, email, password, role } = req.body
 
-        // Check for existing user
-        const existingUser = await User.findOne({ username });
-        if(existingUser) {
-            const response =  res.status(400).json(({ message: 'Username already exists...!' }));
-            console.log(response);
-            return response;
-        }
-
-        // Password hashing
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Create a new user
-        const newUser = new User({ username, email, password: hashedPassword, role });
-        await newUser.save();
-
-        res.status(201).json({ message: 'User registered successfully!' });
-    } catch (error) {
-        console.error('Error registering user:', error);
-        res.status(500).json({ message: 'Internal server error' });
+    // Check for existing user
+    const existingUser = await User.findOne({ username })
+    if (existingUser) {
+      const response = res
+        .status(400)
+        .json({ message: 'Username already exists...!' })
+      console.log(response)
+      return response
     }
-});
 
+    // Password hashing
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    // Create a new user
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      role
+    })
+    await newUser.save()
+
+    res.status(201).json({ message: 'User registered successfully!' })
+  } catch (error) {
+    console.error('Error registering user:', error)
+    res.status(500).json({ message: 'Internal server error' })
+  }
+})
 
 // LOGIN ROUTE
 router.post('/login', async (req, res) => {
-    try {
-        const { email, password } = req.body;
+  try {
+    const { email, password } = req.body
 
-        // fIND USER BY EMAIL
-        const user = User.findOne({ email });
-        if(!user) {
-            const response = res.status(401).json({ message: 'Invalid email or password' });
-            console.log((response));
-            return response;
-        }
-
-        // Compare input password
-        const passwordMatch = await bcrypt.compare(password, user.password);
-        if(!passwordMatch) {
-            const response = res.status(401).json({ message: 'Incorrect password' });
-            console.log(response);
-            return response;
-        }
-
-        // Generate JWT token
-        const token = jwt.sign({ userId: user._id, email: user.email, role: user.role }, process.env.JWT_SECRET_KEY, { expiresIn: '1d' });
-
-        res.status(200).json({ message: 'Login successful', token });
-    } catch (error) {
-        console.error('Error logging in:', error.message);
-        return res.status(500).json({ message: 'Internal server error' });
+    // fIND USER BY EMAIL
+    const user = User.findOne({ email })
+    if (!user) {
+      const response = res
+        .status(401)
+        .json({ message: 'Invalid email or password' })
+      console.log(response)
+      return response
     }
-});
 
-module.exports = router;
+    // Compare input password
+    const passwordMatch = await bcrypt.compare(password, user.password)
+    if (!passwordMatch) {
+      const response = res.status(401).json({ message: 'Incorrect password' })
+      console.log(response)
+      return response
+    }
+
+    // Generate JWT token
+    const token = jwt.sign(
+      { userId: user._id, email: user.email, role: user.role },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: '1d' }
+    )
+
+    res.status(200).json({ message: 'Login successful', token })
+  } catch (error) {
+    console.error('Error logging in:', error.message)
+    return res.status(500).json({ message: 'Internal server error' })
+  }
+})
+
+module.exports = router
