df796a443abeab224bb89c71ae78d21471dc53e5
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 21:18:37 +0100

    Gitacora saved 3 changed files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
     console.log('Stored token:', storedToken);
 
     if(storedToken) {
-      axios.get('/user, {
+      axios.get(', {
         headers: {
           Authorization: `Bearer ${storedToken}`,
         }
