c1aaba5c0f2220192d279da995fb798840210d4f
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 18:09:26 +0100

    Gitacora saved 2742 changed files

--- client/src/components/protectedRoute.jsx
+++ client/src/components/protectedRoute.jsx
@@ -4,7 +4,7 @@ import { useNavigate, Route } from 'react-router-dom';
 
 const ProtectedRoute = ({ component: Component, allowedRoles, ...rest }) => {
     const user = useSelector((state) => state.auth.user);
-    co
+    const 
     return (
         <Route 
             {...rest}
