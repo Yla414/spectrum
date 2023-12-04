044aa69d97453bac21c045c9fdac15b66f405ffe
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 01:38:11 +0100

    Gitacora saved 4 changed files

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -49,7 +49,7 @@ const login = async (req, res) => {
             return res.status(401).json({ error: 'Invalid username or password' });
         }
 
-        const token = genrate
+        const token = generate
     } catch (error) {
         
     }
