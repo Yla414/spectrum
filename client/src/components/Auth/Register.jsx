542180e0c18a847e4817320bfdef3f977ff5eb43
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 03:08:04 +0100

    Gitacora saved 7 changed files

--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -4,7 +4,7 @@ import { register, login } from '../../redux/auth/authSlice';
 
 const Register = () => {
     const dispatch = useDispatch();
-    const [formData, setFormData] = useS
+    const [formData, setFormData] = useState
   return (
     <div>
       
