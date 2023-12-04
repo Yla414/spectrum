96e2dec791db3ef504169dcffc7954f984f8499d
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 03:07:23 +0100

    Gitacora saved 7 changed files

--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
-import { registe }
+import { register } from '../../redux/auth/authSlice';
 
 const Register = () => {
   return (
