6bca2b891ff8f2c3a75cbd3403b94f3ae6d97b62
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 01:07:59 +0100

    Gitacora saved 1 changed files

--- server/src/index.js
+++ server/src/index.js
@@ -16,6 +16,6 @@ mongoose
     .then(() => {
         console.log('MongoDB connection successful!');
         app.listen(process.env.PORT, () => {
-            console.log(`Server connected to http://localhost:${process.e}`)
+            console.log(`Server connected to http://localhost:${process.env.}`)
         })
     })
