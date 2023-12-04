9ab96e2cb76bd6367fc7722f274aa8d592210e6a
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 01:33:59 +0100

    Gitacora saved 4 changed files

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const UserModel = require('../models/user');
 const crypto = require('crypto');
-const bcrypt = require
+const bcrypt = require('')
 
 const generateToken = (user) => {
     const payload = {
