4917196abc2435e45471338678ab62c7eb807d3a
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 04:53:12 +0100

    Gitacora saved 2741 changed files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'rrd'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
+      <BrowserRouter>
       
     </div>
   )
