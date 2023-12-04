2d6caab9a1b3443448ecb3025c7629df7b2a6497
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 02:07:09 +0100

    Gitacora saved 8 changed files

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -2,7 +2,26 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.post('/register', authController.register);
+router.post('/register', async (req, res) => {
+    try {
+        // Validate and hash user password
+        const salt = 10;
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+        const newUser = await UserModel.create({
+            username: req.body.username,
+            password: hashedPassword,
+            email: req.body.email,
+            role: req.body.role,
+        });
+
+        const token = generateToken(newUser);
+        res.status(201).json({ token });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
 router.post('/login', authController.login);
 
 module.exports = router;
