06cc88d4afd194898e2afae3dd4b87227d2d39c3
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 18:07:12 +0100

    Gitacora saved 2742 changed files

--- client/src/components/protectedRoute.jsx
+++ client/src/components/protectedRoute.jsx
@@ -2,4 +2,6 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate, Route } from 'react-router-dom';
 
-const ProtectedRoute = ({ component: Component, allowedRoles, ...rest })
+const ProtectedRoute = ({ component: Component, allowedRoles, ...rest }) => {
+    
+}
