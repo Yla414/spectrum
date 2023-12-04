767c5da1227809dc49a38a64451875782cb0189a
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 19:05:32 +0100

    Gitacora saved 2745 changed files

--- client/src/pages/dashboard/GuestDashboard.jsx
+++ client/src/pages/dashboard/GuestDashboard.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 const Dashboard = () => {
     return (
         <div>
-            <h2>Welcome to the guest dashboard</h2>
-            <p>Your role: {role}</p>
+            <h2>Welcome to the guest dashboard!</h2>
         </div>
     )
 }
