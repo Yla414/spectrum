4b2f8a8797283cc0b4799059287f7b0190b13404
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 20:56:26 +0100

    Gitacora saved 2 changed files

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -9,7 +9,7 @@ const authMiddleware = (req, res, next) => {
 
     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
         if(err) {
-            re
+            return res.stat
         }
     })
 }
