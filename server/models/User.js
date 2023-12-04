924fe51087381f066ab8dc0c5a48072a2897247e
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 03:51:38 +0100

    Gitacora saved 2740 changed files

--- server/models/User.js
+++ server/models/User.js
@@ -4,5 +4,5 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['gues'] }
+    role: { type: String, enum: ['guest', 'seller', ] }
 })
