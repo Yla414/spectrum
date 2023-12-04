2d36e6aa79df9a84c7cd13285af4e0b1c1b521fb
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 04:06:36 +0100

    Gitacora saved 2739 changed files

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const authRoutes = require('./toutes')
+const authRoutes = require('./routes')
 
 // Express app
 const app = express();
