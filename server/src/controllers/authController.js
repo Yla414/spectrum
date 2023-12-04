12e14c5cd68dc0c12bc2f90aa1f04f8f288987e5
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 01:34:40 +0100

    Gitacora saved 4 changed files

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -17,7 +17,7 @@ const register = async (req, res) => {
     try {
         // Validate and hash user password
         const salt = 10;
-        const hashedPassword = await bcr
+        const hashedPassword = await bcrypt
         const newUser = await UserModel.create({
             username: req.body.username,
             email: req.body.email,
