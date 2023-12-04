e8cfc1f290895d9dbc229c3231459a85ea4a0052
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 20:08:04 +0100

    Gitacora saved 2 changed files

--- client/src/components/Logout.jsx
+++ client/src/components/Logout.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { logout } from '../redux/auth/authSlice'
 
+
 const Logout = () => {
     const dispatch = useDispatch();
 
     const handleLogout = () => {
         dispatch(logout());
         localStorage.removeItem('token');
-        
+
     }
   return (
     <div>
