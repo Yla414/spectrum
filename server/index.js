c492f47a4c9d411d985a83f6c2c89bb37f454e28
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 01:13:00 +0100

    Gitacora saved 2 changed files

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 
-const routes = require('./r');
+const routes = require('./');
 
 const app = express();
 dotenv.config();
