433990f4896d08e593dab998af1705b11adbd3b1
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 19:09:06 +0100

    Gitacora saved 2745 changed files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
           <Route path='/' element={<Homepage />}></Route>
           <Route path='/register' element={<Register />}></Route>
           <Route path='/login' element={<Login />}></Route>
+          {/* <Route path='/dashboard' element={<Dashboard />}></Route> */}
         </Routes>
       </BrowserRouter>
     </div>
