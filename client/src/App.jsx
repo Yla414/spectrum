fa80c79b211899faaf0fcfdcf99776883d537be3
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 20:49:14 +0100

    Gitacora saved 2 changed files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
 
   useEffect(() => {
     const storedToken = localStorage.getItem('token');
-    console.log()
+    console.log('storedToken);
 
     if(storedToken) {
       axios.get('/auth/me', {
