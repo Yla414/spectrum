3aae3a4a323f1ff1f0c39b5c7793e4465cee844c
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 21:03:19 +0100

    Gitacora saved 3 changed files

--- server/routes/userDetailRoute.js
+++ server/routes/userDetailRoute.js
@@ -14,7 +14,7 @@ router.get('/me', authMiddleware, async (req, res) => {
         const user = await User.findById(userId);
 
         if(!user) {
-            re
+            return res
         }
     }
 })
