265c756311aa57a10629991a11a50f027f61db96
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 19:59:33 +0100

    Gitacora saved 2 changed files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useSelector, } from 'react-redux'
 import { Homepage, Register, Login, SellerDashboard, CreatorDashboard, GuestDashboard } from './components/components.exports'
 
 const App = () => {
