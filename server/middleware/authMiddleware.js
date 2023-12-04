939aa6da04d6497f7dab12c02e9ef2c7181dfa58
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 20:57:16 +0100

    Gitacora saved 2 changed files

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -12,6 +12,6 @@ const authMiddleware = (req, res, next) => {
             return res.status(401).json({ message: 'Unauthorized. Invalid token' });
         }
 
-        // Attach user detai
+        // Attach user details 
     })
 }
