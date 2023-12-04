f75f54e1a0ceed8211539df3285f8ee4332794cb
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 17:55:24 +0100

    Gitacora saved 2741 changed files

--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -14,7 +14,7 @@ const Register = () => {
     const handleRegister = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:5000/api/auth/login', {
+            const response = await axios.post('auth/login', {
                 email, password
             });
 
