dd65745ed572914e5cde2f002936a9655372709a
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 04:38:20 +0100

    Gitacora saved 2743 changed files

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,1 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./auth/authSlice";
-
-const store = configureStore({
-    reducer: {
-        auth: authReducer,
-    }
-});
-
-export default store;
+import {  }
