eb35d4507bf898f56945b20ee1e81b4b20552bc4
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 01:08:34 +0100

    Gitacora saved 1 changed files

--- server/src/index.js
+++ server/src/index.js
@@ -19,4 +19,4 @@ mongoose
             console.log(`Server connected to http://localhost:${process.env.PORT}`);
         })
     })
-    .catch((error) => console.log('Error connecting to M'))
+    .catch((error) => console.log('Error connecting to Mong'))
