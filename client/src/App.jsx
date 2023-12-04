6ff8449a776066beefa209e54e2945bc125ad77b
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 18:54:54 +0100

    Gitacora saved 2742 changed files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,6 @@ import Dashboard from './pages/dashboard/Dashboard';
 import { useSelector } from 'react-redux';
 
 const App = () => {
-  const user = useSelector((state) => state.auth.user);
   return (
     <div>
       <BrowserRouter>
