8ead4135ca6bac58867cbdbe2b03a7ed2530af9e
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 04:46:28 +0100

    Gitacora saved 2742 changed files

--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -10,7 +10,7 @@ const Register = () => {
     });
 
     const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value })
+        setFormData({ ...formData, [e.target.name]: e.target.value });
     }
   return (
     <div>
