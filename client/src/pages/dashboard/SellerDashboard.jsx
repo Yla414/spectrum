cf21217abf7ae7db785eab03993a8a87b7ae6bb6
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 20:10:39 +0100

    Gitacora saved 2 changed files

--- client/src/pages/dashboard/SellerDashboard.jsx
+++ client/src/pages/dashboard/SellerDashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
-
+import Logout from '../../components/Logout';
 
 const SellerDashboard = () => {
     const user = useSelector((state) => state.auth.user);
