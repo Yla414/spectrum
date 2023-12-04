b071308c38ed54e6f80a487a257ba0e7beed5a1c
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 18:12:07 +0100

    Gitacora saved 2742 changed files

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -5,7 +5,7 @@ const Dashboard = () => {
     const user = useSelector((state) => state.auth.user);
     return (
         <div>
-            <h2>Welcome to the dashboard, {user.username}</h2>
+            <h2>Welcome to the dashboard, {user.username}!</h2>
         </div>
     )
 }
