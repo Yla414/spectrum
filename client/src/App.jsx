ba247b191f69193fba13b01d09372c80e36cab32
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 21:20:05 +0100

    Gitacora saved 3 changed files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import { setToken, setUser, logout } from './redux/auth/authSlice';
 const App = () => {
   const userRole = useSelector(state => state.auth.user?.role);
   const user = useSelector(state => state.auth.user);
-  const username = 
+  const username = user
   const dispatch = useDispatch();
 
   const GetDashboardComponent = () => {
