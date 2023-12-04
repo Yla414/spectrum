4d99d1e231ec532beddd9949429a3e300811028a
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 19:07:32 +0100

    Gitacora saved 2744 changed files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { useSelector } from 're'
+import { useSelector } from 'react-redux';
 import Homepage from './components/Homepage'
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
