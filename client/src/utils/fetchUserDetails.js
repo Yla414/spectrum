82db76c285d8531466e2450f9319d4637ffc0177
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 21:29:17 +0100

    Gitacora saved 4 changed files

--- client/src/utils/fetchUserDetails.js
+++ client/src/utils/fetchUserDetails.js
@@ -3,7 +3,7 @@ import { setUser } from '../redux/auth/authSlice';
 
 const fetchUserDetails = async (dispatch) => {
     try {
-        const 
+        const to
         const response = await axios.get('/user/me', {
             headers: {
                 Authorization: `Bearer ${localStorage.getItem('token')}`
