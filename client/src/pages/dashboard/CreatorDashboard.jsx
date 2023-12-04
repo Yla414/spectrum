462ac45a151d62f96c84d145f826583013c617ec
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 21:15:56 +0100

    Gitacora saved 3 changed files

--- client/src/pages/dashboard/CreatorDashboard.jsx
+++ client/src/pages/dashboard/CreatorDashboard.jsx
@@ -4,7 +4,7 @@ import Logout from '../../components/Logout';
 import fetchUserDetails from '../../utils/fetchUserDetails';
 
 const CreatorDashboard = () => {
-  const 
+  const dispa
   const user = useSelector((state) => state.auth.user);
   return (
     <div>
