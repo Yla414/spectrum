633c4b9531cdb6aa6c126518f5c6d9e8becf5b81
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 01:02:28 +0100

    Gitacora saved 1 changed files

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider>
-      
+    <App />
     </Provider>
   </React.StrictMode>
 );
