6fbf22fd76e48fdc1036f90fd42c5eb96066a902
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 02:52:51 +0100

    Gitacora saved 7 changed files

--- client/src/redux/auth/authSlice.js
+++ client/src/redux/auth/authSlice.js
@@ -12,7 +12,7 @@ const authSlice = createSlice({
     initialState,
     reducers: {
         setUser: (state, action) => {
-            state.user = action.payload
+            state.user = action.payload;
         }
     }
 })
