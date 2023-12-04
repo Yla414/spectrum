96bc415b3c57230ab6b618757a1e96c8bc5fd709
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 03:10:43 +0100

    Gitacora saved 7 changed files

--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -22,7 +22,7 @@ const Register = () => {
         </label>
         
         <label>
-            ema:
+            :
             <input type="text" value={formData.username || ''} onChange={(e) => setFormData({...formData, username: e.target.value})} />
         </label>
 
