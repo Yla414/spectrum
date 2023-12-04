185177ca29a09faf926fd3a40d95c4e69ea9cf90
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 18:08:25 +0100

    Gitacora saved 2742 changed files

--- client/src/components/protectedRoute.jsx
+++ client/src/components/protectedRoute.jsx
@@ -7,7 +7,7 @@ const ProtectedRoute = ({ component: Component, allowedRoles, ...rest }) => {
     return (
         <Route 
             {...rest}
-            render={(p)}
+            render={(props)}
         ></Route>
     )
 }
