b79ddf82cb381d0b9cceb80ba49f6183585e1197
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 04:48:10 +0100

    Gitacora saved 2742 changed files

--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -17,7 +17,7 @@ const Register = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        await axios.post('http:')
+        await axios.post('http://localhost:5000/api/')
     }
   return (
     <div>
