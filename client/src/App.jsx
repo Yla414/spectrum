086d4243c616865d5be82ca2b0b3b0339b1a0431
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 05:02:02 +0100

    Gitacora saved 2741 changed files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Homepage from './components/Homepage';
+import Register from './components/auth/Register'
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <BrowserRouter>
         <Routes></Routes>
           <Route path='/' element={<Homepage />}></Route>
+          <Route path='/register' element={<Register />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
