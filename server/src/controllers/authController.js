7beabf5ce85fa1befba2c8f62f4eda7de0128bc4
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 01:19:14 +0100

    Gitacora saved 4 changed files

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -4,6 +4,6 @@ const UserModel = require('../models/user');
 const generateToken = (user) => {
     const payload: {
         id: user._id,
-        username: user
+        username: user.userna
     }
 }
