df161f95c1a3a70766dacfd47d9e433459c1eba1
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 19:11:25 +0100

    Gitacora saved 2745 changed files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux'
 import Homepage from './components/Homepage'
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
-import GuestDashboard from './pages/dashboard/GuestDashboard';
-import SellerDashboard from './pages/dashboard/SellerDashboard';
-import CreatorDashboard from './pages/dashboard/CreatorDashboard';
+import GuestDashboard from './pages/dashboard/GuestDashboard'
+import SellerDashboard from './pages/dashboard/SellerDashboard'
+import CreatorDashboard from './pages/dashboard/CreatorDashboard'
 
 const App = () => {
+  const userRole = useSelector(state => state.auth.user?.role)
+  const getDashboardComponent = () => {
+    switch (userRole) {
+      case 'Seller':
+        return <SellerDashboard />
+      case 'Creator':
+        return <CreatorDashboard />
+      default:
+        return <GuestDashboard />
+    }
+  }
   return (
     <div>
       <BrowserRouter>
@@ -22,7 +33,5 @@ const App = () => {
     </div>
   )
 }
-
-
 
 export default App
