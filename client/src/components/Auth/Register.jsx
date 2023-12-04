aa65dac7363bacb3f4164252053fd8a415bf24e8
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 17:32:44 +0100

    Gitacora saved 2741 changed files

--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -8,6 +8,8 @@ const Register = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [role, setRole] = useState('Guest');
+
+    
     
   return (
     <div>
