66cf1b789de6a5c924f6b8a8cb340fa19183f27b
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 03:13:00 +0100

    Gitacora saved 7 changed files

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -5,7 +5,7 @@ const Homepage = () => {
   return (
     <div>
       <h1>Homepage</h1>
-      <Link to={'/r'}>Authentication</Link> 
+      <Link to={'/reg'}>Authentication</Link> 
     </div>
   )
 }
