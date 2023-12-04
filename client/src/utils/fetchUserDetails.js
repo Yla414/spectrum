47014eb18e52e79a2af20b203c71680bacf7c028
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 21:13:37 +0100

    Gitacora saved 3 changed files

--- client/src/utils/fetchUserDetails.js
+++ client/src/utils/fetchUserDetails.js
@@ -4,7 +4,7 @@ import { setUser } from '../redux/auth/authSlice';
 const fetchUserDetails = async (dispatch) => {
     try {
         const response = await axios.get('/user', {
-            header
+            headers: {}
         })
     } catch (error) {
         
