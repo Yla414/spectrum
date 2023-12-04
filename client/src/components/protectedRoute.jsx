6582eb4ecbf08ee11bd47fd845e35fa217f2b241
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 18:08:41 +0100

    Gitacora saved 2742 changed files

--- client/src/components/protectedRoute.jsx
+++ client/src/components/protectedRoute.jsx
@@ -8,7 +8,7 @@ const ProtectedRoute = ({ component: Component, allowedRoles, ...rest }) => {
         <Route 
             {...rest}
             render={(props) => {
-                if(user && allowedRoles)
+                if(user && allowedRoles.in)
             }}
         ></Route>
     )
