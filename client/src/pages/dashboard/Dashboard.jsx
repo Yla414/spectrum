0328846fc4765a10352f842a9876623efe89d5b4
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 18:53:13 +0100

    Gitacora saved 2742 changed files

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import useProtectedRoute from '../../hooks/ProtectedRoute'
 
 const Dashboard = () => {
-    const redirect = useProtectedRoute([])
+    const redirect = useProtectedRoute(['Seller', ])
     const user = useSelector((state) => state.auth.user);
     return (
         <div>
