caab35c23a04b61e5fd11ebb37caf5cb04517f2e
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 02:31:46 +0100

    Gitacora saved 6 changed files

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -15,7 +15,7 @@ const generateToken = (user) => {
 
 const register = async (req, res) => {
     try {
-        const { username, email, pas }
+        const { username, email, password } 
         // Validate and hash user password
         const salt = 10;
         const hashedPassword = await bcrypt.hash(req.body.password, salt);
