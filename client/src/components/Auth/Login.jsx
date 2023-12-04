7a6e4ca4b06540b2a6040f3be95ca6ee03eaf0e3
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 19:55:13 +0100

    Gitacora saved 1 changed files

--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -11,7 +11,7 @@ const Register = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    useEffect(() => )
+    useEffect(() => {})
 
     const handleLogin = async (e) => {
         e.preventDefault();
