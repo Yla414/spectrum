7fe45204392c180119f3f726a9fe04e91244df17
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 20:54:47 +0100

    Gitacora saved 2 changed files

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
-    const token = req.headers.authorization && req.headers.authorization.split()
+    const token = req.headers.authorization && req.headers.authorization.split(' ')
 }
