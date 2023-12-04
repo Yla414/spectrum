46320ca054d1aa475a6cba90106c8c4583ffbd1f
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 03:16:19 +0100

    Gitacora saved 9 changed files

--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -6,7 +6,7 @@ const Register = () => {
     const dispatch = useDispatch();
     const [formData, setFormData] = useState({});
 
-    const handleRegister = () => {
+    const handleRegister = (e) => {
         dispatch(register(formData))
     }
 
