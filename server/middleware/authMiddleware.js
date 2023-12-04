8af801bd7a9c174c5ad53cd611918d9a2bb728bd
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 20:55:26 +0100

    Gitacora saved 2 changed files

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -4,6 +4,6 @@ const authMiddleware = (req, res, next) => {
     const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
 
     if(!token) {
-        return res.status(401).json({ message })
+        return res.status(401).json({ message: 'U' })
     }
 }
