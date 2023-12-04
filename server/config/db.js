3b2134fee7260b9524bfe6fd9716094db69e7240
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 03:45:50 +0100

    Gitacora saved 2738 changed files

--- server/config/db.js
+++ server/config/db.js
@@ -4,8 +4,9 @@ const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParse: true,
-            useUnifiedTo
-        })
+            useUnifiedTopology: true,
+        });
+        console.log()
     } catch (error) {
         
     }
