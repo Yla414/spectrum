2b3361f938bb446e7675d2a1cd77566c17828892
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-04 01:36:48 +0100

    Gitacora saved 4 changed files

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -46,7 +46,7 @@ const login = async (req, res) => {
         const isValidPassword = await bcrypt.compare(req.body.password, user.password);
 
         if(!isValidPassword) {
-            return res
+            return res.sta
         }
     } catch (error) {
         
