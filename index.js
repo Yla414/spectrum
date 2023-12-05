d71fa7d6fe5fea8976c39b538234e9a27613e674
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 02:58:18 +0100

    Gitacora saved 37 changed files

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
 const dbConnect = require('./config/dbConnect');
 const authRoutes = require('./routes/authRoute');
-const { errorHandler, notFound } = require('./middlew')
+const { errorHandler, notFound } = require('./middlewares')
 
 // Initialize express app
 const app = express();
