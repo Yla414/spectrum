893a0edabf8e901145540c9fe3e27b4c27d69b09
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 02:46:56 +0100

    Gitacora saved 37 changed files

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3001;
 // Connect database
 dbConnect();
 // 
-app.use(bodyParser.urlencoded {exte});
+app.use(bodyParser.urlencoded {exten});
 app.use('/api/user', authRoutes);
 
 // Start server
