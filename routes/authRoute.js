685acbd9fa9e1ad605478bee7f8d50ee789049e7
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 03:57:11 +0100

    Gitacora saved 37 changed files

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -10,7 +10,7 @@ router.post('/register', createUser);
 router.post('/login', loginUserControl);
 // Ftech all users
 router.get('/all-users', getAllUsers)
-router.get('/all-users', getOneUser)
+router.get('/al', getOneUser)
 
 
 module.exports = router;
