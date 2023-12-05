b852839e5dfae6d5d4bcd14b87d6f6f81c9b834a
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 04:38:42 +0100

    Gitacora saved 38 changed files

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -9,7 +9,7 @@ const authMiddleware = asyncHandler(async(req, res, next) => {
         try {
 
         } catch (error) {
-            throw new Error
+            throw new Error('')
         }
     } else {
         throw new Error('No token provided')
