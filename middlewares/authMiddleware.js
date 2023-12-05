91359c99286eb59bf03942918f1ac71939c59ed2
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 04:56:59 +0100

    Gitacora saved 38 changed files

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -21,6 +21,6 @@ const authMiddleware = asyncHandler(async(req, res, next) => {
     }
 });
 
-const isAdmin = asyncHandler(async())
+const isAdmin = asyncHandler(async(req,))
 
 module.exports = { authMiddleware };
