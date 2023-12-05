4c9a1aa242dcf92e45b9b9f612ccdcd821f579e0
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 03:11:46 +0100

    Gitacora saved 37 changed files

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
