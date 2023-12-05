a8128173f5d31a52d23bcc462798725e917497d1
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 04:06:31 +0100

    Gitacora saved 37 changed files

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -12,6 +12,8 @@ router.post('/login', loginUserControl);
 router.get('/all-users', getAllUsers);
 // Fetch a single user
 router.get('/:id', getOneUser)
+// Fetch a single user
+router.del('/:id', getOneUser)
 
 
 module.exports = router;
