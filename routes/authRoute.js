d11675c4adf982f7f7a1b90c07a6ea4807323e41
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 05:04:09 +0100

    Gitacora saved 36 changed files

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -16,7 +16,7 @@ router.get('/:id', authMiddleware, isAdmin, getOneUser);
 // delete a single user
 router.delete('/:id', deleteUser);
 // Update a user
-router.put('/edit', updateUser);
+router.put('/edit-use', updateUser);
 
 
 module.exports = router;
