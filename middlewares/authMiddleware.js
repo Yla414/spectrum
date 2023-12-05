78f71eeecd472494616a0662bffafd853edc41dc
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 04:55:39 +0100

    Gitacora saved 38 changed files

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,23 @@
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+
+const authMiddleware = asyncHandler(async(req, res, next) => {
+    let token;
+    if(req?.headers?.authorization?.startsWith('Bearer')) {
+        token = req.headers.authorization.split(' ')[1];
+        try {
+            if(token) {
+                const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+                const user = await User.findById(decoded?.id);
+                
+            }
+        } catch (error) {
+            throw new Error('Not authorized. Please login again...!')
+        }
+    } else {
+        throw new Error('No token attached to header')
+    }
+});
+
+module.exports = { authMiddleware };
