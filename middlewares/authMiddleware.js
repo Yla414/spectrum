64306fbd82734edacd048d8d2eaf1faaee5d9dd7
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 04:55:24 +0100

    Gitacora saved 38 changed files

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -9,7 +9,7 @@ const authMiddleware = asyncHandler(async(req, res, next) => {
         try {
             if(token) {
                 const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-                const user = await 
+                const user = await User.f
             }
         } catch (error) {
             throw new Error('Not authorized. Please login again...!')
