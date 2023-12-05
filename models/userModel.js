6e21f366842dd9844f1e09d2b5e1e2a160c3f48c
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 03:13:21 +0100

    Gitacora saved 37 changed files

--- models/userModel.js
+++ models/userModel.js
@@ -27,7 +27,9 @@ var userSchema = new mongoose.Schema({
     },
 });
 
-userSchema.pre('save', async function(next) {})
+userSchema.pre('save', async function(next) {
+    c
+})
 
 
 //Export the model
