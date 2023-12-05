0d95ec5d1db5ad2334578c466e21b53cffe0e8fc
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 02:40:47 +0100

    Gitacora saved 36 changed files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 // Dotenv for env variables
 const dotenv = require('dotenv').config();
-const bobdyParser = re
+const bobdyParser = require
 const dbConnect = require('./config/dbConnect');
 const authRoutes = require('./routes/authRoute');
 
