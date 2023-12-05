a56d35258ec5d9829b3b7d13f95b62c785bd122a
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 02:57:32 +0100

    Gitacora saved 37 changed files

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api/user', authRoutes);
 
 // Pass middlewares
-app.use(notFo)
+app.use(notFound);
 
 // Start server
 app.listen(PORT, () => {
