12d2345d3ec227ec5bf776ee98ab8b2b8c44f27e
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 02:19:48 +0100

    Gitacora saved 34 changed files

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -4,7 +4,7 @@ const uri = process.env.MONGO_URI;
 
 const dbConnect = () => {
     try {
-        console.log('Database connecte')
+        console.log('Database connected')
     } catch (error) {
         console.log('Databse error');
         throw new Error(error);
