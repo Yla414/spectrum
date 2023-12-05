078c142e51d2b424db3d019c5ace2c1123af4194
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 03:27:19 +0100

    Gitacora saved 36 changed files

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,7 +22,7 @@ const loginUserControl = asyncHandler(async(req, res) => {
     // Find if the user is registered
     const findUser = await User.findOne({ email });
     if(findUser && await findUser.isPasswordMatched(password)) {
-        res.json(f)
+        res.json(fin)
     } else {
         throw new Error('Invalid credentials...!')
     }
