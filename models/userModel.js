cfe9d94c90afa94f5f418b2565c605beeb9839a0
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 03:16:27 +0100

    Gitacora saved 37 changed files

--- models/userModel.js
+++ models/userModel.js
@@ -30,7 +30,8 @@ var userSchema = new mongoose.Schema({
 userSchema.pre('save', async function(next) {
     const salt = await bcrypt.genSaltSync(10);
     this.password = await bcrypt.hash(this.password, salt);
-})
+});
+
 
 
 //Export the model
