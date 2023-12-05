ddcceb55e34ecd97f69499b90ea09da5c799cb5f
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 03:13:42 +0100

    Gitacora saved 37 changed files

--- models/userModel.js
+++ models/userModel.js
@@ -28,7 +28,7 @@ var userSchema = new mongoose.Schema({
 });
 
 userSchema.pre('save', async function(next) {
-    const salt = await bcrypt.genSaltSync()
+    const salt = await bcrypt.genSaltSync(10)
 })
 
 
