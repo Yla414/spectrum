2ca95e2d9c5c9322f5404af05d2d9c99c0dc4330
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 04:12:25 +0100

    Gitacora saved 37 changed files

--- controllers/userController.js
+++ controllers/userController.js
@@ -40,7 +40,7 @@ const loginUserControl = asyncHandler(async(req, res) => {
 const updateUser = asyncHandler(async(req, res) => {
     const {id} = req.params;
     try {
-
+        
     } catch (error) {
         throw new Error(error);
     }
