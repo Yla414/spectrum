796d0ea32a7077c496f7f0068813360cc2af6bee
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 03:32:58 +0100

    Gitacora saved 36 changed files

--- config/jwtToken.js
+++ config/jwtToken.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (id) => {
-    return jwt.sign(id, process.env.JWT_SECRET_KEY, { expiresIn: '3' })
+    return jwt.sign(id, process.env.JWT_SECRET_KEY, { expiresIn: '3d' });
+    
 }
