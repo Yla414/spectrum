c58f7abb07ee9251b8b18acad74eae883a9e1dc3
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 03:01:12 +0100

    Gitacora saved 38 changed files

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 const User = require('../models/userModel');
 const asynHandler = require('express-async-handler');
 
-const createUser = a
+const createUser = asyncHandl
 
 module.exports = { createUser };
