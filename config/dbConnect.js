7145b5089bd336c82713a6fec9a5531d0331a8d6
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 02:19:57 +0100

    Gitacora saved 34 changed files

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -4,7 +4,7 @@ const uri = process.env.MONGO_URI;
 
 const dbConnect = () => {
     try {
-        
+        const conn = mongoose.connect(uri);
         console.log('Database connected')
     } catch (error) {
         console.log('Databse error');
