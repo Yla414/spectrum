c5876599d446686ac3b784dcb917cd307c395e43
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 02:55:07 +0100

    Gitacora saved 37 changed files

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -7,5 +7,5 @@ const notFound = (req, res, next) => {
 
 // Error handler for APIs
 const errorHandler = (err, req, res, next) => {
-    const statusCode = res.stat
+    const statusCode = res.statusC
 }
