a9ca8bf8936953a08dd887c89ba63a7234760ee8
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 03:01:08 +0100

    Gitacora saved 38 changed files

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,6 @@
 const User = require('../models/userModel');
 const asynHandler = require('express-async-handler');
 
-const createUser = async (req, res) => {
-    // Check for existing user
-    const email = req.body.email;
-    const findUser = await User.findOne({ email });
-
-    if(!findUser) {
-        // Create a new user if user does not exist
-        const newUser = await User.create(req.body);
-        res.json(newUser);
-    } else {
-        // User alreadyexists
-        res.json({
-            message: 'User already exists',
-            success: false,
-        })
-    };
-};
+const createUser = 
 
 module.exports = { createUser };
