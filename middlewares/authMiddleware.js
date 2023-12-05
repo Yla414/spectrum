f8312393885c77401a02dcf7c945d0ddca9de61d
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 04:36:09 +0100

    Gitacora saved 38 changed files

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,4 +2,4 @@ const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
 const asyncHandler = require('express-async-handler');
 
-const authMiddlewar = asyncHandler(async)
+const authMiddlewar = asyncHandler(async(req, res,))
