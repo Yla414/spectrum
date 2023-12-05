37f74d28dfd7c767dd3efc2a1cfebe6f33a822a1
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 03:17:06 +0100

    Gitacora saved 37 changed files

--- models/userModel.js
+++ models/userModel.js
@@ -32,7 +32,7 @@ userSchema.pre('save', async function(next) {
     this.password = await bcrypt.hash(this.password, salt);
 });
 
-userSchema.methods.isPasswordMatched = async function (enter)
+userSchema.methods.isPasswordMatched = async function (enteredPasswro)
 
 
 //Export the model
