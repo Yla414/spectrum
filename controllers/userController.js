a4500a959ad9b9261ca4313b7ec808c9bfce2eae
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 02:33:51 +0100

    Gitacora saved 37 changed files

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,7 +9,7 @@ const createUser = async (req, res) => {
     if(!user) {
         // Create a new user
         const newUser = User.create(req.body);
-        res.json(n)
+        res.json(nw)
     } else {
         // User alreadyexists
     }
