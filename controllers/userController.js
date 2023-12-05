bb1589235dea45912a967b9045bd88c4d87be960
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 04:11:29 +0100

    Gitacora saved 37 changed files

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,7 +37,7 @@ const loginUserControl = asyncHandler(async(req, res) => {
 });
 
 // Update  user
-const updateUser = asyncHandler(async(req, res))
+const updateUser = asyncHandler(async(req, res) )
 
 
 // Fetch all users
