e2d8797c63611a32c5baae7be6c51fab6bf7228f
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 03:26:47 +0100

    Gitacora saved 36 changed files

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,7 +23,7 @@ const loginUserControl = asyncHandler(async(req, res) => {
     const findUser = await User.findOne({ email });
     if(findUser && await findUser.isPasswordMatched(password)) {
 
-    } 
+    } else 
 })
 
 
