3bc92caf910a3b2a55d9450495273e294e752c38
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 04:37:40 +0100

    Gitacora saved 38 changed files

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -7,6 +7,6 @@ const authMiddleware = asyncHandler(async(req, res, next) => {
     if(req?.headers?.authorization?.startsWith{'Bearer'}) {
         
     } else {
-        throw new Error('No token p')
+        throw new Error('No token prov')
     }
 })
