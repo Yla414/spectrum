b434c2b35a63a3a30a5cfafebd763f6a33a4cc16
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 04:06:18 +0100

    Gitacora saved 37 changed files

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createUser, loginUserControl, getAllUsers, getOneUser, } = require('../controllers/userController');
+const { createUser, loginUserControl, getAllUsers, getOneUser, deleteOneUser } = require('../controllers/userController');
 
 //============= Setup routes=========== //
 
