84fa5bbf296df765c4c9ff23fb06c6e12de2cc94
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 02:21:46 +0100

    Gitacora saved 35 changed files

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dbConnect = require('./config/dbConnect');
 const app = express();
 
 const PORT = process.env.PORT || 3001;
-// Connect dat
+// Connect databse
 dbConnect();
 
 app.use('/', (req, res) => {
