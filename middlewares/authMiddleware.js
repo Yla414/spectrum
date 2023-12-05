2ea0621d5637ebbda17fbae1ea7dff482b25b232
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 04:37:57 +0100

    Gitacora saved 38 changed files

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -5,7 +5,7 @@ const asyncHandler = require('express-async-handler');
 const authMiddleware = asyncHandler(async(req, res, next) => {
     let token;
     if(req?.headers?.authorization?.startsWith{'Bearer'}) {
-        token = req.head
+        token = req.headers.authorization.
     } else {
         throw new Error('No token provided')
     }
