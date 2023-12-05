f243f514539db52e3baae5e2f48b9fc2f3eedb55
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 02:15:26 +0100

    Gitacora saved 34 changed files

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,6 +1,6 @@
 const { default: mongoose } = require("mongoose")
 
-const uri = process.env.MONGO
+const uri = process.env.MONGO_URI
 
 const dbConnect = () => {
     const conn = mongoose.connect()
