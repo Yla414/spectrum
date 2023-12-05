b31dc96bf995181440b2ad72d249e9718f758659
Lukman Yusuf <yusuflukman394@gmail.com>
2023-12-05 04:13:42 +0100

    Gitacora saved 37 changed files

--- controllers/userController.js
+++ controllers/userController.js
@@ -42,7 +42,7 @@ const updateUser = asyncHandler(async(req, res) => {
     try {
         const updateUser = await User.findByIdAndUpdate(id, {
             firstname: req.body.firstname,
-            lastname: req.body.las
+            lastname: req.body.lastname,
         })
     } catch (error) {
         throw new Error(error);
